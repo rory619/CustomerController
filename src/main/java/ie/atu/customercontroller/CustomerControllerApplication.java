dc7bfd7a20783f8e8e01c9fe9724ede50fbd4079
Rory O Loughlin <g00422319@atu.ie>
2024-12-29 16:33:08 +0000

    committing project

---
+++ src/main/java/ie/atu/customercontroller/CustomerControllerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.customercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerControllerApplication.class, args);
+    }
+
+}
